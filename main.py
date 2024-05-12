01764ca349edd149c0090820885cfe95b4007fb9
chenyu0516 <a0981551262@gmail.com>
2024-05-12 14:54:58 +0800

    basic building

---
+++ main.py
@@ -0,0 +1,70 @@
+from huggingface_hub import login
+from transformers import AutoTokenizer, AutoModelForCausalLM
+import transformers
+import torch
+import accelerate
+import os
+
+
+access_token = os.environ['ACCESS_TOKEN']
+login(token = access_token)
+
+MODEL_DICT = {"taide": "taide/TAIDE-LX-7B-Chat", "llama2": "meta-llama/Llama-2-7b-chat-hf"}
+SYS_PROMPT = "Hello! I'm Timothy, your AI Teaching Assistant for the Linear Algebra course. My goal is to help you understand both the concepts and applications of linear algebra and to assist you in solving related problems. Whether you need clarification on topics like vectors, matrices, eigenvalues, or practical applications in engineering and science, feel free to ask me any questions. Let's tackle these mathematical challenges together!\n The following is the chatting record with the current user:"
+SUB_SYS_PROMPT = "Now, you are going to answer the following question:"
+
+class Chat_With:
+    _chat_log = list()
+    _use_pipline = False
+    def __init__(self, model: str, max_chat_log: int, ) -> None:
+        # check
+        if model not in MODEL_DICT.keys:
+            raise(f"KEYERROR: the model you want to chat must chosen from {MODEL_DICT.keys}")
+        if model == "taide":
+            self._use_pipline = True
+        self.model_name = MODEL_DICT[model]
+        self.tokenizer = tokenizer = AutoTokenizer.from_pretrained(model, use_fast=True, token=access_token)
+        self.max_chat_log = max_chat_log
+    
+    def prepare_prompt(self, question:str) -> str:
+        if len(self._chat_log) > self.max_chat_log:
+            chat_log_str = "\n".join(self._chat_log[-self.max_chat_log:])
+        if len(self._chat_log) == 0:
+            chat_log_str = "You haven't talk yet."
+        
+        return "\n".join([SYS_PROMPT, chat_log_str, SUB_SYS_PROMPT, question])
+    
+    def forward(self) -> None:
+        while True:
+            user_input = input("User: ")
+            self.prepare_prompt(user_input)
+            model_inputs = tokenizer(user_input, return_tensors="pt").to("cuda:0")
+            output = model.generate(**model_inputs, max_length=2048)
+            response = f"Timothy: {tokenizer.decode(output[0], skip_special_tokens=True)}"
+            self._chat_log.append(user_input+response)
+            print(response)
+model = "taide/TAIDE-LX-7B-Chat" 
+
+tokenizer = AutoTokenizer.from_pretrained(model)
+pipeline = transformers.pipeline(
+    task = "text-generation",
+    model=model,
+    torch_dtype=torch.float16,
+    device_map={"": 0} # if you have GPU
+)
+
+sequences = pipeline(
+    '你是誰?\n',
+    do_sample=True,
+    top_k=10,
+    top_p = 0.9,
+    temperature = 0.2,
+    num_return_sequences=1,
+    eos_token_id=tokenizer.eos_token_id,
+    truncation=True,
+    max_length = 200, # can increase the length of sequence
+)
+
+print(sequences)
+for seq in sequences:
+    print(f"Result: {seq['generated_text']}")
